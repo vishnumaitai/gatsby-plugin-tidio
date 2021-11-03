137b2878feda2e233edc42850e9cfa4337794f46
vishnumaitai <vishnu@maitaitech.com>
2021-11-03 15:00:11 +0530

    delayInMilliseconds added

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,8 +1,15 @@
 const React = require('react')
 
-exports.onRenderBody = ({ setHeadComponents }, { tidioId, development = true }) => {
+exports.onRenderBody = ({ setHeadComponents }, { tidioId, development = true,delayInMilliseconds=0 }) => {
   let source = "//code.tidio.co/" + tidioId + ".js"
 
+  if(delayInMilliseconds>0){
+    setTimeout(function () {
+      var tidioScript = document.createElement('script');
+      tidioScript.src = source;
+      document.body.appendChild(tidioScript); 
+  }, delayInMilliseconds * 1000);
+  }
   if (!development && process.env.NODE_ENV === 'development') {
     console.log(
       '"development" is set to false - gatsby-plugin-zendesk-chat will not load in development mode',
